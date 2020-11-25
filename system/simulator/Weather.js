acf307528630797d47cd05b5e748089fcd6993db
Peder Johansson <peder96@gmail.com>
2020-11-25 15:35:52 +0100

    Update Weather.js
    simulated wind finished, changed from functions to a class.

--- system/simulator/Weather.js
+++ system/simulator/Weather.js
@@ -1,36 +1,51 @@
+var Normaldist = require("./normaldist");
 
-var data = require('./data/2019_data.json');
-var winds = []; 
-var date;
-//get a random int in a intervall
-function intBetween(start, end){
-    return Math.floor(Math.random()*(end-start)+start)
+class Weather{
+    constructor(){
+        this.data = require('./data/2019_data.json');
+        this.normal = new Normaldist()
+        this.winds = []; 
+        this.date;
+
+    }
+    
+    //get a random int in a intervall
+    intBetween(start, end){
+        return Math.floor(Math.random()*(end-start)+start)
+    }
+    //get a random date from data sort all winds from that date
+    getDate(){
+        this.date = this.data[intBetween(0, this.data.length)].Datum
+        for (var i = 0; i <this.data.length; i++){
+            if (this.data[i].Datum == this.date){
+                this.winds.push(parseFloat(this.data[i].Vindhastighet))
+
+            }
+        }  
+        this.winds = this.winds.sort(function (a, b) {return a - b;});
+    }
+    //get a list of simulated winds for 24h
+    getWind(){
+        simwinds=[];
+        min = this.winds[0]
+        max = this.winds[winds.length-1]
+        wind = intBetween(min, max+1)
+
+        for (var j = 0; j <24; j++){
+            min = wind - 2
+            max = wind + 2
+            if (min<0){
+                min = 0
+            }
+            skew = 1
+            wind = this.normal.randn_bm(min, max, skew)
+            
+            simwinds.push(wind)
+            
+        }  
+        return simwinds
+        
+    }
 }
-//get a random date
-function getDate(){
-    date = data[intBetween(0, data.length)].Datum
-    //return intBetween(0, data.length)
-}
 
-function getWind(){
-    for (var i = 0; i <data.length; i++)
-        if (data[i].Datum == date){
-            winds.push(parseFloat(data[i].Vindhastighet))
-
-        }
-    winds = winds.sort(function (a, b) {  return a - b;  });
-    min = winds[0]
-    max = winds[winds.length-1]
-    wind = intBetween(min, max+1)
-    console.log(min);
-    console.log(max);
-    console.log(wind);
-}
-
-//console.log(getDate());
-getDate()
-getWind()
-console.log(date);
-console.log(winds);
-
-//console.log(typeof(winds[0]));
+module.exports = Weather;
