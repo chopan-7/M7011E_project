f7f7573a4b978a1f5cd2ad483dfadc2cb51a83f1
Peder Johansson <peder96@gmail.com>
2020-11-27 18:09:47 +0100

    production
    power of turbine

--- system/prosumer/prosumer.js
+++ system/prosumer/prosumer.js
@@ -7,9 +7,15 @@ class Prosumer {
     }
 
 
-    //TODO: create production function
+    //TODO: create production function 
     production(wind) {
-        return wind*this.turbine
+        pi = Math.PI
+        r = 58          //radius of turbine
+        v = wind        // wind velocity m/s
+        effi = 0.4      //efficeny %
+        dens = 1.2      //air density 
+        power = (pi/2 * v**3 * r**2 * dens * effi)/1000 //(power of turbine: P = π/2 * r² * v³ * ρ * η) in KW
+        return power
     }
 
     // TODO: buy_ratio function
